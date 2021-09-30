6302292a5414e882969741e8173be01c1b5dd757
Selaelo Sebola <selaelosebola67@gmail.com>
2021-09-30 16:51:19 +0200

    added functionality to register page, updated rank file and updated onSubmitSignIn

--- src/App.js
+++ src/App.js
@@ -35,7 +35,24 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user : {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
   }
 
 calculateFaceLocation = (data) => {
@@ -65,7 +82,22 @@ onButtonSubmit = () => {
     .predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if(response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count }))
+        })
+      }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
     .catch(err => console.log(err));
 };
 
@@ -99,7 +131,7 @@ onRouteChange = (route) => {
             : (
               route === 'signin' 
               ? <Signin onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
