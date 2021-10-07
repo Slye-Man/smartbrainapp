4d33b9eb983a54fa4148cee8a0890285594aa460
Selaelo Sebola <selaelosebola67@gmail.com>
2021-10-07 23:57:38 +0200

    fixed minor error

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -26,8 +26,9 @@ class Signin extends React.Component {
             })
         })
             .then(response => response.json())
-            .then(data => {
-                if (data === 'success') {
+            .then(user => {
+                if (user.id === 'success') {
+                    this.props.loadUser(user)
                     this.props.onRouteChange('home');
                 }
             })
